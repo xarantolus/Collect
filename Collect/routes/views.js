41b3966c780340d3b41c20e4ccdd8e38f5487c23
Philipp Erhardt <philipperhardt29@gmail.com>
2018-01-02 20:56:21 +0100

    Add /new post route

--- Collect/routes/views.js
+++ Collect/routes/views.js
@@ -5,6 +5,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
  */
 const express = require("express");
 const download = require("../tools/download");
+const notif = require("../tools/notifcount");
+const url = require("url");
 const router = express.Router();
 //Show all archived domains
 router.get('/', (req, res) => {
@@ -17,6 +19,66 @@ router.get('/', (req, res) => {
 });
 router.get('/new', (req, res) => {
     res.render('new', { title: "New Entry" });
+});
+router.post('/new', (req, res, next) => {
+    var posted_url = "";
+    try {
+        posted_url = req.body.url;
+        if (posted_url === null || posted_url === undefined || posted_url == "") {
+            throw new Error();
+        }
+    }
+    catch (err) {
+        var err = new Error();
+        err['status'] = 412;
+        err['api'] = true;
+        err.message = "Missing parameter \"url\"";
+        delete err.stack;
+        return next(err);
+    }
+    var depth = 0;
+    try {
+        depth = parseInt(req.body.depth || "0");
+        if (isNaN(depth)) {
+            throw new Error("Malformed parameter \"depth\"");
+        }
+        if (depth > 5) {
+            throw new Error("Parameter \"depth\" is too big");
+        }
+    }
+    catch (errr) {
+        var err = new Error();
+        err['status'] = 412;
+        err['api'] = true;
+        err.message = errr.message;
+        delete err.stack;
+        return next(err);
+    }
+    var parsed;
+    try {
+        parsed = url.parse(posted_url);
+    }
+    catch (err) {
+        err['status'] = 422;
+        err['api'] = true;
+        err.message = "Malformed parameter \"url\"";
+        delete err.stack;
+        return next(err);
+    }
+    res.redirect('/');
+    console.log("Processing url " + posted_url);
+    notif.increaseNotificationCount();
+    req.app.get('socketio').emit('url', { "message": "Started processing url", "step": 0, "url": posted_url, "result": null });
+    download.website(posted_url, depth, function (err, result, fromCache) {
+        notif.decreaseNotificationCount();
+        if (err) {
+            console.log("Error while processing url " + posted_url + ":\n" + err.stack);
+            req.app.get('socketio').emit('url', { "message": "Error while processing url", "step": 4, "url": posted_url, "result": null });
+            return console.log(err);
+        }
+        console.log("Finished url " + posted_url);
+        req.app.get('socketio').emit('url', { "message": "Finished processing url", "step": 2, "url": posted_url, "result": result });
+    });
 });
 exports.default = router;
 //# sourceMappingURL=views.js.map
--- Collect/routes/views.ts
+++ Collect/routes/views.ts
@@ -3,6 +3,8 @@
  */
 import express = require('express');
 import download = require('../tools/download');
+import notif = require('../tools/notifcount');
+import url = require('url');
 const router = express.Router();
 
 //Show all archived domains
@@ -19,5 +21,67 @@ router.get('/new', (req: express.Request, res: express.Response) => {
     res.render('new', { title: "New Entry" });
 });
 
+router.post('/new', (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    var posted_url: string = "";
+    try {
+        posted_url = req.body.url;
+        if (posted_url === null || posted_url === undefined || posted_url == "") {
+            throw new Error();
+        }
+    } catch (err) {
+        var err = new Error();
+        err['status'] = 412;
+        err['api'] = true;
+        err.message = "Missing parameter \"url\"";
+        delete err.stack;
+        return next(err);
+    }
+
+    var depth: number = 0;
+    try {
+        depth = parseInt(req.body.depth || "0");
+        if (isNaN(depth)) {
+            throw new Error("Malformed parameter \"depth\"");
+        }
+        if (depth > 5) {
+            throw new Error("Parameter \"depth\" is too big");
+        }
+    } catch (errr) {
+        var err = new Error();
+        err['status'] = 412;
+        err['api'] = true;
+        err.message = errr.message
+        delete err.stack;
+        return next(err);
+    }
+
+    var parsed: url.Url;
+    try {
+        parsed = url.parse(posted_url);
+    } catch (err) {
+        err['status'] = 422;
+        err['api'] = true;
+        err.message = "Malformed parameter \"url\"";
+        delete err.stack;
+        return next(err);
+    }
+
+    res.redirect('/');
+
+    console.log("Processing url " + posted_url);
+    notif.increaseNotificationCount();
+    req.app.get('socketio').emit('url', { "message": "Started processing url", "step": 0, "url": posted_url, "result": null });
+
+    download.website(posted_url, depth, function (err, result, fromCache) {
+        notif.decreaseNotificationCount();
+        if (err) {
+            console.log("Error while processing url " + posted_url + ":\n" + err.stack);
+            req.app.get('socketio').emit('url', { "message": "Error while processing url", "step": 4, "url": posted_url, "result": null });
+            return console.log(err);
+        }
+        console.log("Finished url " + posted_url)
+        req.app.get('socketio').emit('url', { "message": "Finished processing url", "step": 2, "url": posted_url, "result": result });
+    });
+});
 
 export default router;
