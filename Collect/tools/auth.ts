ee6c6ab4417ecafa2a35210000bb30da2e9189af
Philipp Erhardt <philipperhardt29@gmail.com>
2018-01-14 17:08:40 +0100

    Whitelist resources for login page

--- Collect/tools/auth.ts
+++ Collect/tools/auth.ts
@@ -54,6 +54,14 @@ function generateCookie(cb: (err: Error, cookie: Cookie) => any): void {
     });
 }
 
+
+const resWhitelist = ["/android-icon-144x144.png", "/android-icon-192x192.png", "/android-icon-36x36.png", "/android-icon-48x48.png", "/android-icon-72x72.png", "/android-icon-96x96.png", "/apple-icon-114x114.png", "/apple-icon-120x120.png", "/apple-icon-144x144.png", "/apple-icon-152x152.png", "/apple-icon-180x180.png", "/apple-icon-57x57.png", "/apple-icon-60x60.png", "/apple-icon-72x72.png", "/apple-icon-76x76.png", "/apple-icon-precomposed.png", "/apple-icon.png", "/browserconfig.xml", "/css", "/css/main.css", "/css/uikit-rtl.css", "/css/uikit-rtl.min.css", "/css/uikit.css", "/css/uikit.min.css", "/favicon-16x16.png", "/favicon-32x32.png", "/favicon-96x96.png", "/favicon.ico", "/icon.svg", "/js", "/js/browser.js", "/js/fetch.min.js", "/js/fetch.min.js.map", "/js/promise-7.0.4.min.js", "/js/promise-7.0.4.min.js.map", "/js/socket.io.js", "/js/socket.io.js.map", "/js/uikit-icons.js", "/js/uikit-icons.min.js", "/js/uikit.js", "/js/uikit.min.js", "/manifest.json", "/ms-icon-144x144.png", "/ms-icon-150x150.png", "/ms-icon-310x310.png", "/ms-icon-70x70.png"];
+
+function isResourceRequest(path: string): boolean {
+    return resWhitelist.some(item => path === item);
+}
+
+
 module.exports = function (req: express.Request, res: express.Response, next: express.NextFunction): any {
     var user = null;
     if (req.body && req.body.username && req.body.password) {
@@ -61,6 +69,7 @@ module.exports = function (req: express.Request, res: express.Response, next: ex
     }
 
     var session_cookie = req.cookies["session_id"];
+
 
     if (!session_cookie) {
         if (!user || !(user.name === config.username && user.pass === config.password)) {
@@ -75,9 +84,11 @@ module.exports = function (req: express.Request, res: express.Response, next: ex
                 }
             }
             // Send the login page
-            res.set('WWW-Authenticate', 'Basic realm="auth"');
-            res.status(401).send();
-            return;
+            if (isResourceRequest(req.path)) {
+                return next();
+            } else {
+                return res.status(401).render('login');
+            }
         } else {
             // User can log in
             // We need to generate a cookie for the user
@@ -97,8 +108,10 @@ module.exports = function (req: express.Request, res: express.Response, next: ex
         } else {
             // wrong info, create a new one one
             res.clearCookie("session_id");
-            res.set('WWW-Authenticate', 'Basic realm="auth"');
-            res.status(401).send();
+            if (!req.path.startsWith('/s/')) {
+                return next();
+            }
+            res.status(401).render('login', { error_message: 'The info you provided was not correct' });
             return;
         }
     }
