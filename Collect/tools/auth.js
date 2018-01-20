9945db3bbdc57ed04abadc02bcf54792f48ad5dc
Philipp Erhardt <philipperhardt29@gmail.com>
2018-01-20 11:07:30 +0100

    Don't use deprecated method

--- Collect/tools/auth.js
+++ Collect/tools/auth.js
@@ -61,7 +61,7 @@ module.exports = function (req, res, next) {
             // We don't have a user or wrong info
             if (req.path.startsWith(api_path)) {
                 // This is an api request
-                if (req.param("token") === config.api_token) {
+                if (req.params && req.query.token === config.api_token) {
                     // It is authorized
                     return next();
                 }
