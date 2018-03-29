1b5dfed3b431f49b779214aa76ef133d09209d71
Philipp Erhardt <philipperhardt29@gmail.com>
2018-03-29 23:00:49 +0200

    Set default memory limit to 800mb

--- Collect/start.sh
+++ Collect/start.sh
@@ -1,6 +1,8 @@
 ﻿echo "Running server";
 
 while (true); do
-    node app production >> app.log
+    # The number in the line below sets the maximum memory limit for the JavaScript engine.
+	# If you don't set this option, the maximum size is 512mb
+    node --max-old-space-size=800 app production >> app.log
     echo "Restarting server";
 done
